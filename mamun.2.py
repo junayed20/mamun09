0dedc1cbee654d7ef283cc58e342ce9ce4e0b89d
junayed20 <junayedmamun2@gmail.com>
2025-02-04 22:06:57 +0600

    Add files via upload

---
+++ mamun.2.py
@@ -0,0 +1,17 @@
+#home work 2 string
+
+# mystring = "my name is mamun" 
+# a = mystring.split()
+# print(a)
+# b = "k".join(a)
+# print(b)
+# c = mystring.replace('my',"your")
+# print(c)
+
+
+# hj= "this a big country"
+# print(f"{hj} he is a beautiful")
+
+# price = 30
+# text = f"The price is {price:.2f} dollars"
+# print(text)
